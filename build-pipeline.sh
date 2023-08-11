df1fb4b8c5a9ce7c1c6bff4af0c5ddec53425ebb
robertbotez <robert.botez1996@gmail.com>
2023-08-11 05:25:35 +0300

    Update build-pipeline.sh

--- build-pipeline.sh
+++ build-pipeline.sh
@@ -5,7 +5,7 @@ sudo docker build -t robertbotez/sentiment-analysis-frontend .
 cd ../sa-logic
 sudo docker build -t robertbotez/sentiment-analysis-logic:local .
 cd ../sa-webapp
-sudo docker build -t robertbotez/sentiment-analysis-web-app
+sudo docker build -t robertbotez/sentiment-analysis-web-app .
 cd ..
 
 sudo docker push robertbotez/sentiment-analysis-frontend
